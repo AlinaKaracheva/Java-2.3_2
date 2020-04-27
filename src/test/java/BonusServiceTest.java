3c2cbb06aa16b14d6db2d09ad4ff5826ca383085
Alina Karacheva <alinakaracheva1@mail.ru>
2020-04-27 23:01:19 +0300

    modified pom.xml

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void calculate() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+
+        service.calculate(amount, registered);
+
+    }
+}
+
